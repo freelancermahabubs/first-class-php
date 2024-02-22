1a3afb444d0d6dda97a4a76d78642b499df76c69
mahabube <info.freelancermahabub@gmail.com>
2024-02-22 21:44:16 +0600

    added

--- firstclass.php
+++ firstclass.php
@@ -1,3 +1,32 @@
 <?php
+
+$message = "Hello Jaan";
+echo $message;
+echo "<br>";
 echo "Hello World!";
+echo "<br>";
+echo $message;
+echo "Hello World!";
+echo "<br>";
+// PHP DATA Type 
+$one = "Hello 123"; //String
+$two= 123; // Integer
+$three = 12.5; // Float 
+$four = true; // boolean
+$five=  array(123, "Mahabub Alom"); // Array
+$six  = NULL ; //NULL
+
+echo var_dump($one);
+
+$mahabub_age = 22;
+$habib_age = 22;
+if($mahabub_age < $habib_age) {
+echo "Habib Dadu Bro";
+}
+else if($mahabub_age < $habib_age){
+    echo "Mahabub Dadu Choto";
+}
+else {
+    echo "Both Similar Age";
+}
 ?>
